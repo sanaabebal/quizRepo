de497833373e61e5dcf185f9a428404f5b91db25
domdavid <domdavid@localhost>
2023-11-17 13:32:07 -0800

    initial commit

---
+++ collatzNumber/src/collatzNumber/Runner.java
@@ -0,0 +1,44 @@
+package collatzNumber;
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("5: "+collatzNumber(5));
+	}
+/*
+ * The Collatz conjecture is a mathematical sequence defined as follows: start with any positive integer n. Then, each term is obtained from the previous term according to these rules:
+
+    If the previous term is even, the next term is one half of the previous term.
+    If the previous term is odd, the next term is 3 times the previous term plus 1.
+
+The conjecture is that no matter what value of n, the sequence will always eventually reach the number 1.
+
+Complete the method collatzNumber(int n) which calculates the sequence for the given n.
+For example, below are the resulting sequences for some values of n given the rules above.
+    collatzNumber(1): 1
+    collatzNumber(2): 2 1
+    collatzNumber(3): 3 10 5 16 8 4 2 1
+    collatzNumber(4): 4 2 1
+    collatzNumber(5): 5 16 8 4 2 1
+ */
+	public static String collatzNumber(int n) {
+		//enter solution below
+        String result = _____;
+        
+        while( n _____  ){   
+            result += _____;
+            
+            if( n % 2 = ____   ){ 
+                //even
+                
+            }else{
+                //odd
+                
+            }
+        }
+                
+        return ______;
+	}
+
+}
